644bb818126c405f552f35ed18f275ebc07c3eef
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2022-07-28 18:20:01 +0430

    Token sending implementation steps

--- modules/two_factor_auth/app/Http/Controllers/TokenSenderController.php
+++ modules/two_factor_auth/app/Http/Controllers/TokenSenderController.php
@@ -8,6 +8,12 @@ class TokenSenderController extends Controller
 {
     public function send()
     {
+        $email = request('email');
+        // 1. Stop block users
+        // 2. Generate token
+        // 3. Save token
+        // 4. Send token
+        // 5. Send response
         return 'hello';
     }
 }
