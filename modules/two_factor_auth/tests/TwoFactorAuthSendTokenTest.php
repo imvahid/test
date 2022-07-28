1f2920543a883146ecc790a31f1332e7b40969a1
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2022-07-28 18:13:48 +0430

    Test: TwoFactorAuthSendTokenTest->test_sample added

--- modules/two_factor_auth/tests/TwoFactorAuthSendTokenTest.php
+++ modules/two_factor_auth/tests/TwoFactorAuthSendTokenTest.php
@@ -6,5 +6,9 @@ use Tests\TestCase;
 
 class TwoFactorAuthSendTokenTest extends TestCase
 {
-
+    public function test_sample()
+    {
+        $response = $this->get('/api/two-factor-auth/send');
+        $this->assertTrue($response->content() == 'hello');
+    }
 }
